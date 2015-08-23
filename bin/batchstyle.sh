d5539abb9b3f3b3b4ddfa04016b598581b341550
Marcos Scriven <mscriven@atlassian.com>
2015-08-23 08:51:24 +0100

    Added script to batch process.

--- bin/batchstyle.sh
+++ bin/batchstyle.sh
@@ -2,3 +2,15 @@
 
 echo "Batch processing Armata platform modules."
 
+while read line
+do
+    module=`echo $line | cut -d, -f1`
+    repo=`echo $line | cut -d, -f2`
+    echo "Processing module: $module with repo: $repo"
+
+    # Fetch the branches
+    cat data/platform-module-branches.json |  jq '.modules[] | select(.name=="'$module'") | .branches| join(",")' | tr -d '"' | tr ',' '\n' | while read branch
+    do
+        echo "Processing branch: $branch"
+    done
+done<data/armata-modules.txt
