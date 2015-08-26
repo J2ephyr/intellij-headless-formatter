d66ba4df9a0902f2de638251518266f6ec1791e4
Marcos Scriven <mscriven@atlassian.com>
2015-08-26 13:05:14 +0100

    Not sure why right now, but we need to clear out the headless IntelliJ system/config dir between runs.

--- bin/codestyle.sh
+++ bin/codestyle.sh
@@ -10,5 +10,9 @@ BUILD_DIR=$SCRIPT_DIR/../build
 BUILD_DIR=$(cd $BUILD_DIR; pwd)
 PLUGINS_DIR=$BUILD_DIR/plugins
 
+# Make sure we start with a clean config and system dirname
+rm -rf $BUILD_DIR/idea/config
+rm -rf $BUILD_DIR/idea/system
+
 JAVA_HOME=`/usr/libexec/java_home -v1.8`
 java -Didea.plugins.path=$PLUGINS_DIR -classpath "$JAVA_HOME/lib/*:$BUILD_DIR:$BUILD_DIR/idea/lib/*:$BUILD_DIR/plugins/maven/lib/*:$BUILD_DIR/plugins/properties/lib/*" com.atlassian.codestyle.CodeFormatApplication $POM_PATH
