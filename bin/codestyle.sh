180f202e0f8c2eb0bf9d9cb59a2c24afd1c71187
G. Richard Bellamy <rbellamy@terradatum.com>
2015-11-07 17:03:47 -0800

    Setup remote debugging

--- bin/codestyle.sh
+++ bin/codestyle.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Mac or Linux
 PLATFORM=$(uname -s)
@@ -21,4 +21,7 @@ rm -rf ${BUILD_DIR}/idea/system
 [[ "$PLATFORM" == "Darwin" ]] && JHOME=$(/usr/libexec/java_home -v1.8) || JHOME=/usr/lib/jvm/java-8-jdk
 JAVA_HOME="$JHOME"
 
-java -Didea.plugins.path=${PLUGINS_DIR} -classpath "$JAVA_HOME/lib/*:$BUILD_DIR:$BUILD_DIR/idea/lib/*:$BUILD_DIR/plugins/maven/lib/*:$BUILD_DIR/plugins/properties/lib/*" com.atlassian.codestyle.CodeFormatApplication ${POM_PATH}
+# NO DEBUG
+#java -Didea.plugins.path=${PLUGINS_DIR} -classpath "$JAVA_HOME/lib/*:$BUILD_DIR:$BUILD_DIR/idea/lib/*:$BUILD_DIR/plugins/maven/lib/*:$BUILD_DIR/plugins/properties/lib/*" com.atlassian.codestyle.CodeFormatApplication ${POM_PATH}
+# DEBUG
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 -Didea.plugins.path=${PLUGINS_DIR} -classpath "$JAVA_HOME/lib/*:$BUILD_DIR:$BUILD_DIR/idea/lib/*:$BUILD_DIR/plugins/maven/lib/*:$BUILD_DIR/plugins/properties/lib/*" com.atlassian.codestyle.CodeFormatApplication ${POM_PATH}
